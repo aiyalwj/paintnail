b8803d47f7809191e667fb65cd6c6859a8e02c41
clouds <2270056326@qq.com>
2022-12-21 16:15:15 +0800

    yxf2分支

--- src/main/java/com/lwj/PaintNail/entity/Myinfo.java
+++ src/main/java/com/lwj/PaintNail/entity/Myinfo.java
@@ -20,6 +20,18 @@ public class Myinfo implements Serializable {
     private String userId;
 
     /**
+     *
+     */
+    @TableField(value = "User_headportrait")
+    private String user_Headportrait;
+
+    /**
+     *
+     */
+    @TableField(value = "User_background")
+    private String user_Background;
+
+    /**
      * 
      */
     @TableField(value = "User_account")
@@ -45,6 +57,22 @@ public class Myinfo implements Serializable {
      */
     public String getUserId() {
         return userId;
+    }
+
+    public String getUser_Headportrait() {
+        return user_Headportrait;
+    }
+
+    public void setUser_Headportrait(String user_Headportrait) {
+        this.user_Headportrait = user_Headportrait;
+    }
+
+    public String getUser_Background() {
+        return user_Background;
+    }
+
+    public void setUser_Background(String user_Background) {
+        this.user_Background = user_Background;
     }
 
     /**
